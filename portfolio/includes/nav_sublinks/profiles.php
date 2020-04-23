4a566338002b16dd4b839bc44f029099d246e6e7
Mario Morales <sniper_ix@hotmail.com>
2020-04-23 21:52:55 +0000

    another fix on navbar links

--- portfolio/includes/nav_sublinks/profiles.php
+++ portfolio/includes/nav_sublinks/profiles.php
@@ -1,22 +1,14 @@
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {Profiles}
+          {Profiles}
     </a>
     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="https://www.linkedin.com/in/mario-morales-developer/">LinkedIn</a>
+        <a class="dropdown-item" href="https://www.linkedin.com/in/mario-morales-developer/" target="_blank">LinkedIn</a>
     <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="https://github.com/mariomorales123456">Github Profile</a>
-    </div>
-    <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="https://mariomoralesportfolio.herokuapp.com/portfolio/index.php">Working Branch Website</a>
-    </div>
-    <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="https://mariomoralesprojects.000webhostapp.com/index.html">Master Branch Website</a>
-    </div>
-    <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="https://www.sololearn.com/Profile/10653809">SoloLearn</a>
-    </div>
-            <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="https://www.freecodecamp.org/mario_morales">FCC</a>
+        <a class="dropdown-item" href="https://github.com/mariomorales123456" target="_blank">Github Profile</a>
+        <a class="dropdown-item" href="https://mariomoralesportfolio.herokuapp.com/portfolio/index.php" target="_blank">Working Branch Website</a>
+        <a class="dropdown-item" href="https://mariomoralesprojects.000webhostapp.com/index.html" target="_blank">Master Branch Website</a>
+        <a class="dropdown-item" href="https://www.sololearn.com/Profile/10653809" target="_blank">SoloLearn</a>
+        <a class="dropdown-item" href="https://www.freecodecamp.org/mario_morales" target="_blank">FCC</a>
     </div>
 </li>
