4e1b9c0c4df899fb78dcad3503c2fad9fc834549
Mario Morales <sniper_ix@hotmail.com>
2020-06-12 01:11:56 +0000

    updated root index with a commented task list to keep organized

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
 <?php header("Location: portfolio/index.php"); ?>
+
+<?php 
+
+/*To Do List*/
+
+/*Website task list:
+
+Gather images and descriptions:
+Wordpress - Done!
+Java - Done!
+Android - Done!
+C++ - not done yet
+Linux - not done yet
+
+Actualize website with images and descriptions:
+Wordpress - not done yet
+Java - not done yet
+Android  - not done yet
+C++ - not done yet
+Linux - not done yet
+
+*/
+?>
