c643d29d1afdc1a5ff6960de82ff96cededb8410
Mario Morales <sniper_ix@hotmail.com>
2020-06-16 15:45:08 +0000

    deleted cosw10 folder, fixed nav link path error cosw10_20

--- portfolio/includes/nav_sublinks/cosw10_20.php
+++ portfolio/includes/nav_sublinks/cosw10_20.php
@@ -5,7 +5,7 @@
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="/portfolio/cosw10_20/index.php">Home</a>
         <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="/portfolio/cosw10_20/index.php#final_project">5 Page Website</a>
+                <a class="dropdown-item" href="/portfolio/cosw10_20/index.php#cosw10_final_project">5 Page Website</a>
                 <a class="dropdown-item" href="/portfolio/cosw10_20/index.php#technical_document">Technical Document</a>
                 <a class="dropdown-item" href="/portfolio/cosw10_20/index.php#survey_form">Survey Form</a>
                 <a class="dropdown-item" href="/portfolio/cosw10_20/index.php#landing_page">Landing Page</a>
