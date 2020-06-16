6b898262a5fdc1dd249ab418baa040a174e788ba
Mario Morales <sniper_ix@hotmail.com>
2020-06-16 14:01:42 +0000

    rearranged cosw200 js jquery index.php, removed reflections, streamlined with php includes

--- portfolio/includes/nav_sublinks/cosw200.php
+++ portfolio/includes/nav_sublinks/cosw200.php
@@ -5,17 +5,15 @@
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="/portfolio/cosw200/index.php">Home</a>
         <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework10">Form Validation</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework9">Filtering, Searching &amp; Sorting</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework8">Content Panels</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework7">AJAX &amp; JSON</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework6">JS Events</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework1">Input &amp; Output</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework2">Functions</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework3">Strings &amp; Arrays</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework4">Objects</a>
             <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework5">DOM</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework4">JS Objects</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework3">JS Strings &amp; Arrays</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework2">JS Functions</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework1">JS Input &amp; Output</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#research">Research Project</a>
-            <a class="dropdown-item" href="/portfolio/cosw200/index.php#codewithme">Activity 1-3</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework6">Listener Events</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework7">AJAX &amp; JSON</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework8">Content Panels</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework9">Filtering, Searching &amp; Sorting</a>
+            <a class="dropdown-item" href="/portfolio/cosw200/index.php#homework10">Form Validation</a>
         </div>
       </li>
