1f15af1cd2e236c5dde9342fe80e5795ba762424
Mario Morales <sniper_ix@hotmail.com>
2020-06-17 10:54:08 +0000

    testing raw code and carousel pages

--- portfolio/cosp201_230/cosp230_raw_code/week12_final/index.php
+++ portfolio/cosp201_230/cosp230_raw_code/week12_final/index.php
@@ -3,95 +3,58 @@
 <!-- //////////////  XML Activities   ///////////////////// -->
 
 <div class="raw_code_container">
-<h1 class="text-center">
-    XMl Activities
-</h1>
-
-<div class="raw_code_container">
-    <?php include('activity_date_picker_xml.php'); ?>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('activity_gridview_xml.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('activity_main_xml.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('activity_radio_timer_play_pause_xml.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('splash_activit_.xml.php'); ?>
-</pre>
-</div>
+    <h1 class="text-center">
+        XMl Activities
+    </h1>
+    <pre class="border-1 rounded">
+        <?php include('activity_date_picker_xml.php'); ?>
+    </pre>
+    <pre>
+        <?php include('activity_gridview_xml.php'); ?>
+    </pre>
+    <pre>
+        <?php include('activity_main_xml.php'); ?>
+    </pre>
+    <pre>
+        <?php include('activity_radio_timer_play_pause_xml.php'); ?>
+    </pre>
+    <pre>
+        <?php include('splash_activit_.xml.php'); ?>
+    </pre>
 
 <!-- //////////////  XML Utilities   ///////////////////// -->
 
-<h1 class="text-center">
-    XMl Utilities
-</h1>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('colors_xml.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('strings_xml.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('styles_xml.php'); ?>
-</pre>
-</div>
+    <h1 class="text-center">
+        XMl Utilities
+    </h1>
+    <pre>
+        <?php include('colors_xml.php'); ?>
+    </pre>
+    <pre>
+        <?php include('strings_xml.php'); ?>
+    </pre>
+    <pre>
+        <?php include('styles_xml.php'); ?>
+    </pre>
 
 <!-- //////////////  Java   ///////////////////// -->
 
-<h1 class="text-center">
-    Java code
-</h1>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('activity_main_java.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('date_picker_java.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('gridview_java.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('radio_timer_play_pause_java.php'); ?>
-</pre>
-</div>
-
-<div class="raw_code_container">
-<pre>
-    <?php include('splash_activity_java.php'); ?>
-</pre>
-</div>
+    <h1 class="text-center">
+        Java code
+    </h1>
+    <pre>
+        <?php include('activity_main_java.php'); ?>
+    </pre>
+    <pre>
+        <?php include('date_picker_java.php'); ?>
+    </pre>
+    <pre>
+        <?php include('gridview_java.php'); ?>
+    </pre>
+    <pre>
+        <?php include('radio_timer_play_pause_java.php'); ?>
+    </pre>
+    <pre>
+        <?php include('splash_activity_java.php'); ?>
+    </pre>
 </div>
--- portfolio/cosp201_230/images/cosp230_final/image_carousel.php
+++ portfolio/cosp201_230/images/cosp230_final/image_carousel.php
@@ -2,98 +2,100 @@
 
 
 <div class="raw_code_container">
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="11"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="12"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="13"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="14"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="15"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="16"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="17"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="18"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="19"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="final1.PNG" alt="image1 slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="final2.PNG" alt="image2 slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="final3.PNG" alt="image3 slide">
-    </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="11"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="12"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="13"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="14"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="15"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="16"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="17"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="18"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="19"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img class="img-fluid" src="final1.PNG" alt="image1 slide">
+        </div>
         <div class="carousel-item">
-      <img class="d-block w-100" src="final4.PNG" alt="image4 slide">
-    </div>
+        <img class="img-fluid" src="final2.PNG" alt="image2 slide">
+        </div>
         <div class="carousel-item">
-      <img class="d-block w-100" src="final5.PNG" alt="image5 slide">
+        <img class="img-fluid" src="final3.PNG" alt="image3 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final4.PNG" alt="image4 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final5.PNG" alt="image5 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final6.PNG" alt="image6 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final7.PNG" alt="image7 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final8.PNG" alt="image8 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final9.PNG" alt="image9 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final10.PNG" alt="image10 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final11.PNG" alt="image11 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final12.PNG" alt="image12 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final13.PNG" alt="image13 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final14.PNG" alt="image14 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final15.PNG" alt="image15 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final16.PNG" alt="image16 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final17.PNG" alt="image17 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final18.PNG" alt="image18 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final19.PNG" alt="image19 slide">
+        </div>
+            <div class="carousel-item">
+        <img class="img-fluid" src="final20.PNG" alt="image120 slide">
+        </div>
     </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final6.PNG" alt="image6 slide">
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
     </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final7.PNG" alt="image7 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final8.PNG" alt="image8 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final9.PNG" alt="image9 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final10.PNG" alt="image10 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final11.PNG" alt="image11 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final12.PNG" alt="image12 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final13.PNG" alt="image13 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final14.PNG" alt="image14 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final15.PNG" alt="image15 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final16.PNG" alt="image16 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final17.PNG" alt="image17 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final18.PNG" alt="image18 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final19.PNG" alt="image19 slide">
-    </div>
-        <div class="carousel-item">
-      <img class="d-block w-100" src="final20.PNG" alt="image120 slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-</div>
+
+<?php include('../../../includes/javascript.php'); ?>
