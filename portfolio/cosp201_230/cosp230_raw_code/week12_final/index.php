e03ab5067feb5a0fc1f88c8dee3e8a645407280e
Mario Morales <sniper_ix@hotmail.com>
2020-06-17 12:14:11 +0000

    xml display testing

--- portfolio/cosp201_230/cosp230_raw_code/week12_final/index.php
+++ portfolio/cosp201_230/cosp230_raw_code/week12_final/index.php
@@ -3,60 +3,60 @@
 <!-- //////////////  XML Activities   ///////////////////// -->
 
 <div class="raw_code_container">
-    <div class="col text-center">
-        <div class="row text-center">
+    <div class="col">
+        <div class="row">
             <h1>XMl Activities</h1>
         </div>
         <div class="row">
-            <pre class="text-left"><?php echo htmlentities(file_get_contents('activity_date_picker_xml.php')); ?></pre>
+            <pre><?php echo htmlentities(file_get_contents('activity_date_picker_xml.php')); ?></pre>
         </div>
         <div class="row">
-            <pre class="text-left"><?php echo htmlentities(file_get_contents('activity_gridview_xml.php')); ?></pre>
+            <pre><?php echo htmlentities(file_get_contents('activity_gridview_xml.php')); ?></pre>
         </div>
         <div class="row">
-             <pre class="text-left"><?php echo htmlentities(file_get_contents('activity_main_xml.php')); ?></pre>
+             <pre><?php echo htmlentities(file_get_contents('activity_main_xml.php')); ?></pre>
         </div>
         <div class="row">
-            <pre class="text-left"><?php echo htmlentities(file_get_contents('activity_radio_timer_play_pause_xml.php')); ?></pre>
+            <pre><?php echo htmlentities(file_get_contents('activity_radio_timer_play_pause_xml.php')); ?></pre>
         </div>
         <div class="row">
-            <pre class="text-left"><?php echo htmlentities(file_get_contents('splash_activit_.xml.php')); ?></pre>
+            <pre><?php echo htmlentities(file_get_contents('splash_activit_.xml.php')); ?></pre>
         </div> 
 
 <!-- //////////////  XML Utilities   ///////////////////// -->
 
-        <div class="row text-center">
+        <div class="row">
             <h1>XMl Utilities</h1>
         </div>
-        <div class="row text-left">
-            <pre class="text-left"><?php echo htmlentities(file_get_contents('colors_xml.php')); ?></pre>
+        <div class="row">
+            <pre><?php echo htmlentities(file_get_contents('colors_xml.php')); ?></pre>
         </div>
-        <div class="row text-left">
-            <pre class="text-left"><?php echo htmlentities(file_get_contents('strings_xml.php')); ?></pre>
+        <div class="row">
+            <pre><?php echo htmlentities(file_get_contents('strings_xml.php')); ?></pre>
         </div>
-        <div class="row text-left">
-             <pre class="text-left"><?php echo htmlentities(file_get_contents('styles_xml.php')); ?></pre>
+        <div class="row">
+             <pre><?php echo htmlentities(file_get_contents('styles_xml.php')); ?></pre>
         </div>
 
 <!-- //////////////  Java   ///////////////////// -->
 
-        <div class="row text-center">
+        <div class="row">
             <h1>Java code</h1>
         </div>
         <div class="row">
-             <pre class="text-left"><?php include('activity_main_java.php'); ?></pre>
+             <pre><?php echo htmlentities(file_get_contents('activity_main_java.php')); ?></pre>
         </div>
         <div class="row">
-             <pre class="text-left"><?php include('date_picker_java.php'); ?></pre>
+             <pre><?php echo htmlentities(file_get_contents('date_picker_java.php')); ?></pre>
         </div>
         <div class="row">
-             <pre class="text-left"><?php include('gridview_java.php'); ?></pre>
+             <pre><?php echo htmlentities(file_get_contents('gridview_java.php')); ?></pre>
         </div>
         <div class="row">
-             <pre class="text-left"><?php include('radio_timer_play_pause_java.php'); ?></pre>
+             <pre><?php echo htmlentities(file_get_contents('radio_timer_play_pause_java.php')); ?></pre>
         </div>
         <div class="row">
-             <pre class="text-left"><?php include('splash_activity_java.php'); ?></pre>
+             <pre><?php echo htmlentities(file_get_contents('splash_activity_java.php')); ?></pre>
         </div>
     </div>
 </div>
