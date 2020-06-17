6f15d55bdec856c23caf769d7c64c194e1f93e60
Mario Morales <sniper_ix@hotmail.com>
2020-06-17 12:17:03 +0000

    xml display testing

--- portfolio/cosp201_230/cosp230_raw_code/week12_final/index.php
+++ portfolio/cosp201_230/cosp230_raw_code/week12_final/index.php
@@ -4,58 +4,58 @@
 
 <div class="raw_code_container">
     <div class="col">
-        <div class="row">
+        <div class="row justify-content-center">
             <h1>XMl Activities</h1>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
             <pre><?php echo htmlentities(file_get_contents('activity_date_picker_xml.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
             <pre><?php echo htmlentities(file_get_contents('activity_gridview_xml.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
              <pre><?php echo htmlentities(file_get_contents('activity_main_xml.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
             <pre><?php echo htmlentities(file_get_contents('activity_radio_timer_play_pause_xml.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
             <pre><?php echo htmlentities(file_get_contents('splash_activit_.xml.php')); ?></pre>
         </div> 
 
 <!-- //////////////  XML Utilities   ///////////////////// -->
 
-        <div class="row">
+        <div class="row justify-content-center">
             <h1>XMl Utilities</h1>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
             <pre><?php echo htmlentities(file_get_contents('colors_xml.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
             <pre><?php echo htmlentities(file_get_contents('strings_xml.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
              <pre><?php echo htmlentities(file_get_contents('styles_xml.php')); ?></pre>
         </div>
 
 <!-- //////////////  Java   ///////////////////// -->
 
-        <div class="row">
+        <div class="row justify-content-center">
             <h1>Java code</h1>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
              <pre><?php echo htmlentities(file_get_contents('activity_main_java.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
              <pre><?php echo htmlentities(file_get_contents('date_picker_java.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
              <pre><?php echo htmlentities(file_get_contents('gridview_java.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
              <pre><?php echo htmlentities(file_get_contents('radio_timer_play_pause_java.php')); ?></pre>
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
              <pre><?php echo htmlentities(file_get_contents('splash_activity_java.php')); ?></pre>
         </div>
     </div>
