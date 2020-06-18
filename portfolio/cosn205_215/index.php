15ab868b10d61b6c6c812f7205824c320a5b4d47
Mario Morales <sniper_ix@hotmail.com>
2020-06-18 10:00:29 +0000

    added images and content for cosn205 215 linux unix security

--- portfolio/cosn205_215/index.php
+++ portfolio/cosn205_215/index.php
@@ -3,10 +3,140 @@
 <main>
 
 <div class="gradient_title">
-       <h1>UNIX/Fundemtanls &amp; LINUX Networking and Security</h1>
+       <h1>UNIX/Fundamentals &amp; LINUX Networking and Security</h1>
 </div>
 
-____SECTIONS_GO_HERE_______
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="unix_fundamentals" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">unix_fundamentals</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/cosn205_final.PNG" target="_blank"><img class="img-fluid" src="images/cosn205_final.PNG" alt="first page of cosn205 final pdf"></a><figcaption class="text-center">Click <a href="pdfs/cosn205_final.pdf" target="_blank">here</a> to see PDF file</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->                                   <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="cosn215_project1" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">Virtual DMZ</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/cosn215_project1.PNG" target="_blank"><img class="img-fluid" src="images/cosn215_project1.PNG" alt="first page of cosn215 project 1 pdf"></a><figcaption class="text-center">Click <a href="pdfs/cosn215_project1.pdf" target="_blank">here</a> to see PDF file</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->                                   <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="cosn215_project2" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">SSH &amp; Apache Web Server</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/cosn215_project2.PNG" target="_blank"><img class="img-fluid" src="images/cosn215_project2.PNG" alt="first page of cosn215 project 2 pdf"></a><figcaption class="text-center">Click <a href="pdfs/cosn215_project2.pdf" target="_blank">here</a> to see PDF file</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->                                   <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="cosn215_project3" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">IDS &amp; Snort</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/cosn215_project3.PNG" target="_blank"><img class="img-fluid" src="images/cosn215_project3.PNG" alt="first page of cosn215 project 3 pdf"></a><figcaption class="text-center">Click <a href="pdfs/cosn215_project3.pdf" target="_blank">here</a> to see PDF file</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->                                   <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="cosn215_project4" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">Pam, GRUB &amp; AppArmor</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/cosn215_project4.PNG" target="_blank"><img class="img-fluid" src="images/cosn215_project4.PNG" alt="first page of cosn215 project 4 pdf"></a><figcaption class="text-center">Click <a href="pdfs/cosn215_project4.pdf" target="_blank">here</a> to see PDF file</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->                                   <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="cosn215_project5" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">File System, Samba, Quotas &amp; Remote Logging</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/cosn215_project5.PNG" target="_blank"><img class="img-fluid" src="images/cosn215_project5.PNG" alt="first page of cosn215 project 5 pdf"></a><figcaption class="text-center">Click <a href="pdfs/cosn215_project5.pdf" target="_blank">here</a> to see PDF file</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->                                   <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
 
 </main>
     
--- portfolio/includes/nav_sublinks/cosn205_215.php
+++ portfolio/includes/nav_sublinks/cosn205_215.php
@@ -5,6 +5,11 @@
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="/portfolio/cosn205_215/index.php">Home</a>
         <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="___ABSOLUTE_PATH_____">___NAME___</a>
+            <a class="dropdown-item" href="/portfolio/cosn205_215/index.php#unix_fundamentals">Unix Fundamentals</a>
+            <a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project1">Virtual DMZ</a>
+            <a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project2">SSH &amp; Apache Web Server</a>
+            <a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project3">IDS &amp; Snort</a>
+            <a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project4">Pam, GRUB &amp; AppArmor</a>
+            <a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project5">File System, Samba, Quotas &amp; Remote Logging</a>
         </div>
       </li>
