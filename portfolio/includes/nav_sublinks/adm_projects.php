8f1fdaa39876468a0c3da4c3fecdf8c08eb1ea8c
Mario Morales <sniper_ix@hotmail.com>
2022-01-28 18:35:15 +0000

    boostrap and jquery were out of date, updated, changed title of nav bar

--- portfolio/includes/nav_sublinks/adm_projects.php
+++ portfolio/includes/nav_sublinks/adm_projects.php
@@ -1,6 +1,6 @@
 <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle active" href="/portfolio/adm_projects/index.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          ADM Projects
+          {ADM Projects}
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="___ABSOLUTE_PATH_____">Coming Soon!</a>
