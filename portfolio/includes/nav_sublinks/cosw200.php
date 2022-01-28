dac7002ca7aab1ebd8cde2bbac356180bcbe3287
Mario Morales <sniper_ix@hotmail.com>
2022-01-28 20:34:59 +0000

    removed =

--- portfolio/includes/nav_sublinks/cosw200.php
+++ portfolio/includes/nav_sublinks/cosw200.php
@@ -1,4 +1,4 @@
-=
+
       <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">{jQuery/JavaScript}</a>
     <ul class="dropdown-menu">
