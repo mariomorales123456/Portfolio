c09659ba606bf9f8dc0d83b02b8498a33ffa2887
Mario Morales <sniper_ix@hotmail.com>
2022-06-28 21:51:19 +0000

    folder restructure

--- portfolio/includes/nav_sublinks/cosn205_215.php
+++ portfolio/includes/nav_sublinks/cosn205_215.php
@@ -1,13 +1,15 @@
+<?php $root =  "/portfolio/projects/cosn205_215/unix-fundementals-and-linux-networking-and-security.php" ?>
+
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">{UNIX/Linux}</a>
     <ul class="dropdown-menu">
         <li><a class="dropdown-item" href="/portfolio/cosn205_215/index.php">UNIX/Linux Home</a></li>
         <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="/portfolio/cosn205_215/index.php#unix_fundamentals">Unix Fundamentals</a></li>
-        <li><a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project1">Virtual DMZ</a></li>
-        <li><a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project2">SSH &amp; Apache Web Server</a></li>
-        <li><a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project3">IDS &amp; Snort</a></li>
-        <li><a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project4">Pam, GRUB &amp; AppArmor</a></li>
-        <li><a class="dropdown-item" href="/portfolio/cosn205_215/index.php#cosn215_project5">File System, Samba, Quotas &amp; Remote Logging</a></li>
+        <li><a class="dropdown-item" href="<?php echo $root ?>#unix_fundamentals">Unix Fundamentals</a></li>
+        <li><a class="dropdown-item" href="<?php echo $root ?>#cosn215_project1">Virtual DMZ</a></li>
+        <li><a class="dropdown-item" href="<?php echo $root ?>#cosn215_project2">SSH &amp; Apache Web Server</a></li>
+        <li><a class="dropdown-item" href="<?php echo $root ?>#cosn215_project3">IDS &amp; Snort</a></li>
+        <li><a class="dropdown-item" href="<?php echo $root ?>#cosn215_project4">Pam, GRUB &amp; AppArmor</a></li>
+        <li><a class="dropdown-item" href="<?php echo $root ?>#cosn215_project5">File System, Samba, Quotas &amp; Remote Logging</a></li>
     </ul>
 </li>
--- portfolio/includes/navigation.php
+++ portfolio/includes/navigation.php
@@ -13,46 +13,9 @@
             include('nav_sublinks/cosp201_230.php'); 
             include('nav_sublinks/cs11.php'); 
             include('nav_sublinks/cosn205_215.php');
-            include('nav_sublinks/freelance_projects.php');
-            include('nav_sublinks/adm_projects.php');
+            include('nav_sublinks/freelance.php');
+            include('nav_sublinks/adm.php');
             include('nav_sublinks/profiles.php');
         ?>
     </ul>
 </nav>
-
-
-
-<?php 
-/*
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="navbar">
-    <a class="navbar-brand" href="/portfolio/index.php">Home</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto nav nav-tabs">
-
-            <?php 
-                include('nav_sublinks/cosw10_20.php'); 
-                include('nav_sublinks/cosp38.php'); 
-                include('nav_sublinks/cosw200.php');
-                include('nav_sublinks/cosw30.php');  
-                include('nav_sublinks/cosw240.php'); 
-                include('nav_sublinks/cs21_22.php'); 
-                include('nav_sublinks/cosp201_230.php'); 
-                include('nav_sublinks/cs11.php'); 
-                include('nav_sublinks/cosn205_215.php');
-                include('nav_sublinks/recent_projects.php');
-                include('nav_sublinks/adm_projects.php');
-            ?>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-         </div>
-         </nav>
-
-*/
-?>
