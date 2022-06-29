077df34fdf4077bc73a21ff9518b63ba1a807715
Mario Morales <sniper_ix@hotmail.com>
2022-06-29 19:54:47 +0000

    profile link css test

--- portfolio/projects/profiles/index.php
+++ portfolio/projects/profiles/index.php
@@ -6,11 +6,11 @@
 
 <div class="gradient_title">
        <h1>External Profiles</h1>
-    <ul>
-        <li><a class="text-black" href="https://www.linkedin.com/in/mario-morales-developer/" target="_blank">LinkedIn</a></li>
-        <li><a class="text-black" href="https://github.com/mariomorales123456" target="_blank">Github Profile</a></li>
-        <li><a class="text-black" href="https://www.sololearn.com/Profile/10653809" target="_blank">SoloLearn</a></li>
-        <li><a class="text-black" href="https://www.freecodecamp.org/mario_morales" target="_blank">FCC</a></li>
+    <ul class="list-style--none">
+        <li><a class="text-black hover_underline_animation" href="https://www.linkedin.com/in/mario-morales-developer/" target="_blank">LinkedIn</a></li>
+        <li><a class="text-black hover_underline_animation" href="https://github.com/mariomorales123456" target="_blank">Github Profile</a></li>
+        <li><a class="text-black hover_underline_animation" href="https://www.sololearn.com/Profile/10653809" target="_blank">SoloLearn</a></li>
+        <li><a class="text-black hover_underline_animation" href="https://www.freecodecamp.org/mario_morales" target="_blank">FCC</a></li>
     </ul>
 </div>
 
