ea5776db3c1c6b122ee755cc63df4ac592b7f852
Mario Morales <sniper_ix@hotmail.com>
2022-06-29 18:31:52 +0000

    js script testing

--- portfolio/includes/custom_js.js
+++ portfolio/includes/custom_js.js
@@ -7,6 +7,7 @@ function stickymenu(){
     windowScrollPosition.scroll(function () {
         if (windowScrollPosition.scrollTop() > 120) {
             menuElement.addClass("sticky_menu");	
+            console.log("test2");
         } 
         else {
             menuElement.removeClass("sticky_menu");
@@ -20,6 +21,7 @@ function stickymenu(){
 
 $(document).ready(function(){
     
+    console.log("test");
 
     stickyMenu();
     
