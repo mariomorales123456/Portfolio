9b21ae7322abcb7a4129e556e688e36274d17730
Mario Morales <sniper_ix@hotmail.com>
2022-07-01 21:24:40 +0000

    adm page changes

--- portfolio/projects/adm/index.php
+++ portfolio/projects/adm/index.php
@@ -196,6 +196,48 @@
 
 
 <!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="image_grid_2" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">Image Grid 2</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/front_page_6.jpg" target="_blank"><img class="img-fluid" src="images/front_page_6.jpg" alt="icon grid"></a><figcaption class="text-center">Click <a href="admflooring.com" target="_blank">here</a> to go to site</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->
+        <div class="col">                            <!-- /*right column start*/ -->
+            <div class="requirements">
+                <h3 class="text-center">Details</h3>  
+                <p>Issues:</p>
+                <p><ul>
+                    <li>Previous grid was created using raw HTML/CSS and the source images were from other websites</li>
+                    <li>Assymetrical and off-center</li>
+                </ul></p>
+                <p>Fixes:</p>
+                <p><ul>
+                    <li>Screenshot the icons to use through own server</li>
+                    <li>Use oxygen editor to manage the styling of the grid</li>
+                    <li>Flexbox controls to align</li>
+                </ul></p>
+            </div>
+        </div>                                       <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+    
+
+
+
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
 <section id="__________INSERT_ID_HERE_________" class="container">
     <div class="row">                                                     
         <div class="col">
@@ -222,10 +264,62 @@
     </div>                                                                   <!-- /*content row end*/-->
 </section>                                                               
 <!--///////////////////////// SECTION END ///////////////////////////////////--> 
-    
 
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="__________INSERT_ID_HERE_________" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">______INSERT_TITLE_HERE________</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="__INSERT_IMAGE_HERE__" target="_blank"><img class="img-fluid" src="__INSERT_IMAGE_HERE_" alt="__INSERT_ALT_HERE__"></a><figcaption class="text-center">Click <a href="__INSERT_WEBSITE_HERE__" target="_blank">here</a> to go to site</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->
+        <div class="col">                            <!-- /*right column start*/ -->
+            <div class="requirements">
+                <h3 class="text-center">Details</h3>  
+                <p>__INSERT_LIST_HERE__</p>
+            </div>
+        </div>                                       <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
 
-
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="__________INSERT_ID_HERE_________" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">______INSERT_TITLE_HERE________</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="__INSERT_IMAGE_HERE__" target="_blank"><img class="img-fluid" src="__INSERT_IMAGE_HERE_" alt="__INSERT_ALT_HERE__"></a><figcaption class="text-center">Click <a href="__INSERT_WEBSITE_HERE__" target="_blank">here</a> to go to site</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->
+        <div class="col">                            <!-- /*right column start*/ -->
+            <div class="requirements">
+                <h3 class="text-center">Details</h3>  
+                <p>__INSERT_LIST_HERE__</p>
+            </div>
+        </div>                                       <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
 
 <!--///////////////////////// SECTION START ///////////////////////////////////-->   
 <section id="__________INSERT_ID_HERE_________" class="container">
