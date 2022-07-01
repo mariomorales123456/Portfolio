b5c3458d17c002b5f4a040d1a4491543e1840abf
Mario Morales <sniper_ix@hotmail.com>
2022-07-01 18:42:57 +0000

    adm page changes

--- portfolio/projects/adm/index.php
+++ portfolio/projects/adm/index.php
@@ -46,6 +46,68 @@
 <!--///////////////////////// SECTION END ///////////////////////////////////--> 
 
 <!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="banner_ad" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">Banner Ad</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/front_page_2.jpg" target="_blank"><img class="img-fluid" src="images/front_page_2.jpg" alt="banner ad"></a><figcaption class="text-center">Click <a href="admflooring.com" target="_blank">here</a> to go to site</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->
+        <div class="col">                            <!-- /*right column start*/ -->
+            <div class="requirements">
+                <h3 class="text-center">Requirements</h3>  
+                <p>The issue: Visual customers were not exploring the nav menu</p>
+                <p>Fixes:</p>
+                <p><ul>
+                    <li>Took highlights from the navbar to redirect customers to specific pages</li>
+                    <li>Scroll only customers would see a picture visual with a link redirect that would entice them to continue on the website</li>
+                    <li>Hover effects to make it interactive</li>
+                </ul></p>
+            </div>
+        </div>                                       <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="__________INSERT_ID_HERE_________" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">______INSERT_TITLE_HERE________</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="__INSERT_IMAGE_HERE__" target="_blank"><img class="img-fluid" src="__INSERT_IMAGE_HERE_" alt="__INSERT_ALT_HERE__"></a><figcaption class="text-center">Click <a href="__INSERT_WEBSITE_HERE__" target="_blank">here</a> to go to site</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->
+        <div class="col">                            <!-- /*right column start*/ -->
+            <div class="requirements">
+                <h3 class="text-center">Requirements</h3>  
+                <p>__INSERT_LIST_HERE__</p>
+            </div>
+        </div>                                       <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
 <section id="__________INSERT_ID_HERE_________" class="container">
     <div class="row">                                                     
         <div class="col">
