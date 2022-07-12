50b60d8906f82e9d8450c9f80872dc536501e35e
Mario Morales <sniper_ix@hotmail.com>
2022-07-12 22:25:48 +0000

    nav update

--- portfolio/includes/nav_sublinks/adm.php
+++ portfolio/includes/nav_sublinks/adm.php
@@ -1,13 +1,13 @@
-<?php $index =  $root . 'adm/index.php' ?>
+<?php $index =  $root . 'adm/' ?>
 
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">{<span class="hover_underline_animation">ADM Projects</span>}</a>
     <ul class="dropdown-menu">
       <li><a class="dropdown-item" href="<?php echo $index ?>"><span class="hover_underline_animation">ADM Projects Home</span></a></li>
       <li><hr class="dropdown-divider"></li>
-      <li><a class="dropdown-item" href="___ABSOLUTE_PATH_____"><span class="hover_underline_animation">___NAME___</span></a></li>
-      <li><a class="dropdown-item" href="___ABSOLUTE_PATH_____"><span class="hover_underline_animation">___NAME___</span></a></li>
-      <li><a class="dropdown-item" href="___ABSOLUTE_PATH_____"><span class="hover_underline_animation">___NAME___</span></a></li>
-      <li><a class="dropdown-item" href="___ABSOLUTE_PATH_____"><span class="hover_underline_animation">___NAME___</span></a></li>
+      <li><a class="dropdown-item" href="<?php echo $index ?>flooring_front_end/index.php"><span class="hover_underline_animation">Flooring Front-End</span></a></li>
+      <li><a class="dropdown-item" href="<?php echo $index ?>flooring_back_end/index.php"><span class="hover_underline_animation">Flooring Back-End</span></a></li>
+
+      <!--<li><a class="dropdown-item" href="___ABSOLUTE_PATH_____"><span class="hover_underline_animation">___NAME___</span></a></li>-->
     </ul>
   </li>
