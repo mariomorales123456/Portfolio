eb3e4271468974d5308df9b5dbd9fd3a9822a9db
Mario Morales <sniper_ix@hotmail.com>
2022-07-14 22:46:54 +0000

    new section flooring back end

--- portfolio/projects/adm/flooring_back_end/index.php
+++ portfolio/projects/adm/flooring_back_end/index.php
@@ -193,10 +193,10 @@
 <!--///////////////////////// SECTION END ///////////////////////////////////--> 
 
 <!--///////////////////////// SECTION START /////////////////////////////////// #6 -->   
-<section id="__________INSERT_ID_HERE_________" class="container">
+<section id="add_custom_field_radio_button_2_choices_checkout" class="container">
     <div class="row">                                                     
         <div class="col">
-            <h2 class="text-center">______INSERT_TITLE_HERE________</h2>  <!-- /*title row*/ -->
+            <h2 class="text-center">Ckechout Custom Field - Radio Button</h2>  <!-- /*title row*/ -->
         </div>
     </div> 
     <div class="row">                                                     <!-- /*content row start*/-->
@@ -205,7 +205,7 @@
                 <div class="col">
                     <h3 class="text-center">Preview</h3>  
                     <figure class="text-center">
-                        <a href="code_images/add_custom_field_radio_button_2_choices_checkout.png" target="_blank"><img class="img-fluid" src="code_images/add_custom_field_radio_button_2_choices_checkout.png" alt="__INSERT_ALT_HERE__"></a><figcaption class="text-center">Click <a href="__INSERT_WEBSITE_HERE__" target="_blank">here</a> to go to site</figcaption>
+                        <a href="code_images/add_custom_field_radio_button_2_choices_checkout.png" target="_blank"><img class="img-fluid" src="code_images/add_custom_field_radio_button_2_choices_checkout.png" alt="image of back end code"></a><figcaption class="text-center">Click <a href="admflooring.com/checkout" target="_blank">here</a> to go to site</figcaption>
                     </figure>
                 </div>
             </div>    
@@ -215,16 +215,15 @@
                 <h3 class="text-center">Details</h3>  
                 <p>Issues:</p>
                 <p><ul>
-                    <li>__TEXT__</li>
+                    <li>We need more information from clients when they are placing an order, but without adding more plugins to get this functionality</li>
                 </ul></p>
                 <p>Fixes:</p>
                 <p><ul>
-                    <li>__TEXT___</li>
-                    <li>__TEXT___</li>
-                    <li>__TEXT___</li>
-                    <li>__TEXT___</li>
-                    <li>__TEXT___</li>
-                    <li>__TEXT___</li>
+                    <li>Woocommerce has several PHP hooks to get the functionality needed</li>
+                    <li>Combining several hooks we can add the information to the metadata, use it and display it</li>
+                    <li>Furthermore, we want to reuse this snippet of code</li>
+                    <li>Using a PHP object to encapsulate the hooks, and then a secondary function to put together the variables servers this purpose</li>
+                    <li>Using this format I created 3 different options blocks for a client to go through</li>
                 </ul></p>
             </div>
         </div>                                       <!-- /*right column end*/ -->   
