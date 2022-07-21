d4b8c366ef5a210ea3804afae4325ebd0c99416c
Mario Morales <sniper_ix@hotmail.com>
2022-07-21 20:51:04 +0000

    added images and links to villagio

--- portfolio/projects/adm/villagio/index.php
+++ portfolio/projects/adm/villagio/index.php
@@ -23,7 +23,85 @@
                 <div class="col">
                     <h3 class="text-center">Preview</h3>  
                     <figure class="text-center">
-                        <a href="__imageurl___" target="_blank"><img class="img-fluid" src="__imageurl___" alt="__ALT__"></a><figcaption class="text-center">Click <a href="__Urlwebsite___" target="_blank">here</a> to go to site</figcaption>
+                        <a href="images/villagio_accessories.jpg" target="_blank"><img class="img-fluid" src="images/villagio_accessories.jpg" alt="villagio accessory page"></a><figcaption class="text-center">Click <a href="__Urlwebsite___" target="_blank">here</a> to go to site</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->
+        <div class="col">                            <!-- /*right column start*/ -->
+        <div class="requirements">
+                <h3 class="text-center">Details</h3>  
+                <p>Issues:</p>
+                <p><ul>
+                    <li>__issues___</li>
+                </ul></p>
+                <p>Fixes:</p>
+                <p><ul>
+                    <li>__fixes__</li>
+                    <li>__fixes__</li>
+                    <li>__fixes__</li>
+                    <li>__fixes__</li>
+                    <li>__fixes__</li>
+                 </ul></p>
+            </div>
+        </div>                                       <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="__id__" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">___title___</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/villagio_header.jpg" target="_blank"><img class="img-fluid" src="images/villagio_header.jpg" alt="image of villagio header"></a><figcaption class="text-center">Click <a href="__Urlwebsite___" target="_blank">here</a> to go to site</figcaption>
+                    </figure>
+                </div>
+            </div>    
+        </div>                                       <!-- /*left column end*/ -->
+        <div class="col">                            <!-- /*right column start*/ -->
+        <div class="requirements">
+                <h3 class="text-center">Details</h3>  
+                <p>Issues:</p>
+                <p><ul>
+                    <li>__issues___</li>
+                </ul></p>
+                <p>Fixes:</p>
+                <p><ul>
+                    <li>__fixes__</li>
+                    <li>__fixes__</li>
+                    <li>__fixes__</li>
+                    <li>__fixes__</li>
+                    <li>__fixes__</li>
+                 </ul></p>
+            </div>
+        </div>                                       <!-- /*right column end*/ -->   
+    </div>                                                                   <!-- /*content row end*/-->
+</section>                                                               
+<!--///////////////////////// SECTION END ///////////////////////////////////--> 
+
+<!--///////////////////////// SECTION START ///////////////////////////////////-->   
+<section id="__id__" class="container">
+    <div class="row">                                                     
+        <div class="col">
+            <h2 class="text-center">___title___</h2>  <!-- /*title row*/ -->
+        </div>
+    </div> 
+    <div class="row">                                                     <!-- /*content row start*/-->
+        <div class="col">                          <!-- /*left column start*/ -->
+            <div class="row preview">     <!--/* image container*/ -->
+                <div class="col">
+                    <h3 class="text-center">Preview</h3>  
+                    <figure class="text-center">
+                        <a href="images/villagio_see_in_my_room_button.jpg" target="_blank"><img class="img-fluid" src="images/villagio_see_in_my_room_button.jpg" alt="image of villagio button"></a><figcaption class="text-center">Click <a href="__Urlwebsite___" target="_blank">here</a> to go to site</figcaption>
                     </figure>
                 </div>
             </div>    
